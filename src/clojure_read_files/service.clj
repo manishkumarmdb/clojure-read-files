9b941b44f9289bed785a4f6a4af7402a807a6db5
manishkumarmdb <manishkumarmdb@gmail.com>
2017-02-10 11:13:41 +0530

    update path to atom path in all functions where need @path

--- src/clojure_read_files/service.clj
+++ src/clojure_read_files/service.clj
@@ -21,36 +21,41 @@
   without any loss of formatting (newline) around paragraphs.")
 
 ;; define path for accessing files from system.
-(defonce path "/home/manish/manish/essays")
+;;(defonce path "/home/manish/manish/essays")
+(def path (atom ""))
 
-(def get-files-from-path (file-seq (io/file path)))
+(defn get-all-inside-dir [dir-path]
+  (file-seq (io/file dir-path)))
 
 (defn only-files
   "Filter a sequence of files/directories by the .isFile property of
   java.io.File"
-  [file-s]
-  (filter #(.isFile %) file-s))
+  [files-seq]
+  (filter #(.isFile %) files-seq))
 
-(defn names
+(defn file-names
   "Return the .getName property of a sequence of files"
-  [file-s]
-  (map #(.getName %) file-s))
+  [files-seq]
+  (map #(.getName %) files-seq))
 
-(defn get-files-name []
-  (sort
-   (-> get-files-from-path
-       only-files
-       names)))
+(defn seq-file-name
+  "Return only files name with lexical order"
+  []
+  (sort (file-names (only-files (get-all-inside-dir @path)))))
 
-(defn get-file-contents [path file-name]
-  (with-open [rdr (io/reader (str path "/" file-name)
+(defn get-file-contents
+  "Return contents inside perticular file"
+  [dir-path file-name]
+  (with-open [rdr (io/reader (str dir-path "/" file-name)
                              :append true
                              :encoding "UTF-8")]
     #_(doseq [line (line-seq rdr)]
         (println line))
     (doall (line-seq rdr))))
 
-(defn answer-hiccup [path]
+(defn answer-hiccup
+  ""
+  []
   (hiccup/html ""
                [:html
                 [:head
@@ -62,18 +67,18 @@
                   [:table.table-table
                    [:thead
                     [:tr
-                     (for [hd (take 5 (get-files-name))] ;; take only 5 files max
+                     (for [hd (take 5 (seq-file-name))] ;; take only 5 files max
                        ^{:key hd}
                        [:th.table-th
                         (first (clj-string/split hd #"[.]")) ;; getting file name without extension
                         ])]]
                    [:tbody
                     [:tr
-                     (for [file-name (take 5 (get-files-name))] ;; take only 5 file's contents max
+                     (for [file-name (take 5 (seq-file-name))] ;; take only 5 file's contents max
                        ^{:key file-name}
                        [:td.table-td
                         [:div.table-td-div
-                         (for [line (get-file-contents path file-name)]
+                         (for [line (get-file-contents @path file-name)]
                            ^{:key line}
                            [:p line])
                          #_ (slurp (str path "/" file-name))
@@ -81,7 +86,9 @@
                    ]]]]
                ))
 
-(defn ques-ans-hiccup [path]
+(defn ques-ans-hiccup
+  ""
+  []
   (hiccup/html ""
                [:html
                 [:head
@@ -94,7 +101,7 @@
                   [:div
                    [:h4 "Answer."]]
                   [:div
-                   (answer-hiccup path)]]]]))
+                   (answer-hiccup)]]]]))
 
 
 ;; generate map with file-name and it's contents
@@ -103,20 +110,19 @@
    :file-contents (into [] file-contents-seq)})
 
 ;; storing map of file-names and it's contents in vector
-(defn data [path]
+(defn data []
   (loop [return-data []
-         files       (get-files-name)]
+         files       (seq-file-name)]
     (if (empty? files)
       return-data
       (recur (conj return-data
                    (file-to-map (first files)
-                                (get-file-contents path
-                                                   (first files))))
+                                (get-file-contents @path (first files))))
              (rest files)))))
 
 ;; manually generate JSON for file and its contents
-(defn data-to-json [path]
-  (generate-string (data path)
+(defn data-to-json []
+  (generate-string (data)
                    {:pretty true}))
 
 ;; define html response for success.
@@ -139,7 +145,7 @@
 (defn home-page
   [req]
   (html-response
-   (ques-ans-hiccup path)))
+   (ques-ans-hiccup)))
 
 ;; Set up routes to get all the above handlers accessible.
 (def routes
